31c8678ce7eaad3404f4d059c75713b2d27ccfb9
ShantanuK24 <shantanu.khokrale@arieotech.com>
2025-02-12 13:07:59 +0530

    Created Models for feedback and daily meals (#4)
    Co-authored-by: HarshalWagh0901 <harshal.wagh@arieotech.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
 
-     
+
   self.inheritance_column = :type  # STI will use 'role' column
-        
-  validates :type, presence: true
-        
+
+  # validates :type , presence: true
+
   def admin?
     type == "Admin"
   end
-        
+
   def employee?
     type == "Employee"
   end
-        
 end
