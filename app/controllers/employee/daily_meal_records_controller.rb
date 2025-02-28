430fd63c58cb4193b489b7ac4113b786c7533aaa
sudha-official <sudha.shanmugam@areiotech.com>
2025-02-28 13:21:25 +0530

    meal Access_time UTC time was checked

--- app/controllers/employee/daily_meal_records_controller.rb
+++ app/controllers/employee/daily_meal_records_controller.rb
@@ -1,6 +1,6 @@
 class Employee::DailyMealRecordsController < ApplicationController
   before_action :authenticate_user!
-    # before_action :check_access_time, only: [:create] # Restrict access based on time
+    before_action :check_access_time, only: [:create] # Restrict access based on time
     # GET /daily_meal_records/new
     def new
       @daily_meal_record = DailyMealRecord.new
@@ -34,11 +34,14 @@ class Employee::DailyMealRecordsController < ApplicationController
     end
 
     def check_access_time
-      current_hour = Time.now.hour
-      current_min = Time.now.min
+      current_time = Time.now.utc.in_time_zone("Asia/Kolkata")
+      formatted_time = current_time.strftime("%I:%M %p") # Display time in 12-hour format with AM/PM
+      current_hour = current_time.hour
+      current_min = current_time.min
 
-      unless current_hour == 12 || (current_hour == 13 && current_min == 0) # Allows from 12:00 to 12:59 PM
-        redirect_to new_employee_daily_meal_record_path, alert: "You can only access meals between 12:00 PM and 1:00 PM."
+      unless current_hour == 12 || (current_hour == 13 && current_min == 0) # Allows from 12:00 to 12:59 PM IST
+         redirect_to new_employee_daily_meal_record_path, alert: "You can only access meals between 12:00 PM and 1:00 PM IST. Current time: #{formatted_time}."
       end
     end
+    
 end
